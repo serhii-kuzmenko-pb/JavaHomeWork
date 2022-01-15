eb3520d3c3b2df226b3f3b525124fb4b6facb839
Sergey Kuzmenko <sergej.kuzmenko@privatbank.ua>
2022-01-15 23:37:11 +0200

    HomeWork №14. Echo server

---
+++ src/com/pb/kuzmenko/hw14/MultiEchoClient.java
@@ -0,0 +1,38 @@
+package com.pb.kuzmenko.hw14;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MultiEchoClient {
+
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", 1111);
+        Scanner scan = new Scanner(System.in);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+        System.out.println("Введите сообщение: ");
+        new Thread(() -> {
+            while (true) {
+                String message = scan.nextLine();
+                writer.println(message);
+            }
+        }).start();
+
+        String fromServer;
+        while ((fromServer = reader.readLine()) != null) {
+            System.out.println(ANSI_GREEN + fromServer + ANSI_RESET);
+            System.out.println("Введите сообщение: ");
+        }
+
+        System.out.println("Соединение с сервером прервано");
+        System.exit(0);
+    }
+}
